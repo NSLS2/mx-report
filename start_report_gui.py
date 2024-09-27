80fc3a7148a7e5ba6ff7dee121eed5e578553785
vshekar1 <vshekar1@bnl.gov>
2024-09-27 15:41:19 -0400

    Using paths to load correct data

--- start_report_gui.py
+++ start_report_gui.py
@@ -7,11 +7,12 @@ import argparse
 from pathlib import Path
 import sys
 import yaml
+import pickle
 
-def start_app(config_path):
+def start_app(config_path, collection_data, current_directory):
     app = QtWidgets.QApplication(sys.argv)
     # app.setWindowIcon(QIcon(str(Path.cwd() / Path("gui/assets/icon.png"))))
-    ex = MainWindow(config_path=config_path)
+    ex = MainWindow(config_path=config_path, collection_data=collection_data, data_path = current_directory)
     ex.show()
     sys.exit(app.exec_())
 
@@ -22,7 +23,7 @@ def init_argparse() -> argparse.ArgumentParser:
     )
 
     parser.add_argument(
-        "-v", "--version", action="version", version=f"{parser.prog} version 1.0.0"
+        "-v", "--version", action="version", version=f"{parser.prog} version 0.0.1"
     )
     parser.add_argument("--config", help="yaml file containing the configuration")
     parser.add_argument(
@@ -30,6 +31,12 @@ def init_argparse() -> argparse.ArgumentParser:
         dest="beamline",
         help="importer for the beamline (MX or LIX), default is MX",
         default="MX",
+    )
+    parser.add_argument(
+        "--report-dir",
+        dest="report_dir",
+        help="Optionally specify the directory where the report directory is",
+        default="amg_report"
     )
     return parser
 
@@ -46,8 +53,23 @@ def main() -> None:
             f"Configuration file {config_path} does not exist, please provide a valid config path"
         )
         return
+    current_directory = Path.cwd()
+    report_directory = current_directory / Path(args.report_dir)
+    data_directory = report_directory / Path("data")
+    database_json_file = data_directory / Path("data.json")
+    data_pickle_file = data_directory / Path("data.pickle")
 
-    start_app(config_path)
+    if not data_pickle_file.exists():
+        print(f"Report data file not found at {data_pickle_file.absolute()}, please run the report script")
+        return
+    
+    try:
+        with data_pickle_file.open('rb') as f:
+            collection_data = pickle.load(f)
+    except Exception as e:
+        print("Error importing data file: {e}.\nRecommend rerunning report script")
+
+    start_app(config_path, collection_data, current_directory)
 
 
 if __name__ == "__main__":
