e9549a3c0e481660df928fffb6d25f3dadddcb22
vshekar1 <vshekar1@bnl.gov>
2024-10-02 16:40:46 -0400

    Added result models

--- utils/models.py
+++ utils/models.py
@@ -1,7 +1,7 @@
-from pydantic import BaseModel, validator, Field, root_validator
+from pydantic import BaseModel, field_validator, Field, model_validator
 from uuid import UUID
 from datetime import datetime
-from typing import Optional, Literal, Union
+from typing import Optional, Literal, Union, Dict, Any, List, Tuple
 from enum import Enum
 from pathlib import Path
 
@@ -18,9 +18,9 @@ class BaseRequestDefinition(BaseModel):
     img_width: float
     exposure_time: float
     protocol: RequestType
-    detector_distance: float = Field(..., alias="detDist")
-    parent_request_id: UUID | Literal[-1]
-    base_path: Path
+    detector_distance: float = Field(..., alias="detDist", validate_default=True)
+    parent_request_id: UUID | Literal[-1] = Field(..., alias="parentReqID")
+    base_path: Path = Field(..., alias="basePath", validate_default=True)
     file_prefix: str
     directory: Path
     file_number_start: int
@@ -39,20 +39,20 @@ class BaseRequestDefinition(BaseModel):
     pos_type: str
     grid_step: float = Field(..., alias="gridStep")
     run_num: int = Field(..., alias="runNum")
-    xbeam: float
-    ybeam: float
+    xbeam: Optional[float] = None
+    ybeam: Optional[float] = None
+    model_config = {"frozen": False, "extra": "allow"}
 
-    class Config:
-        extra = "allow"  # Allow extra fields to pass into the model
-
-    @root_validator(pre=True)
+    @model_validator(mode="before")
     def ignore_specific_fields(cls, values):
         # Removing dataPath because it is always stuck in /legacy/2022-3 which is of no use
         fields_to_ignore = {"dataPath"}
-        return {k: v for k, v in values.items() if k not in fields_to_ignore}
+        if isinstance(values, dict):
+            return {k: v for k, v in values.items() if k not in fields_to_ignore}
+        return values
 
     # Validator for fields of type `Path`
-    @validator("base_path", "directory", pre=True, always=True)
+    @field_validator("base_path", "directory")
     def convert_to_path(cls, value):
         return Path(value) if value else value
 
@@ -61,21 +61,31 @@ class StandardRequestDefinition(BaseRequestDefinition):
     centering_option: Literal["AutoRaster", "Manual"] = Field(
         ..., alias="centeringOption"
     )
-    fast_dp: bool
-    fast_ep: bool
+    fast_dp: bool = Field(..., alias="fastDP")
+    fast_ep: bool = Field(..., alias="fastEP")
     dimple: bool
     xia2: bool
+    model_config = {"frozen": False}
 
 
 class Point2D(BaseModel):
     x: float
     y: float
+    model_config = {"frozen": False}
+
+
+class Point3D(BaseModel):
+    x: float
+    y: float
+    z: float
+    model_config = {"frozen": False}
 
 
 class RasterRowDefinition(BaseModel):
     start: Point2D
     end: Point2D
     num_steps: int = Field(..., alias="numsteps")
+    model_config = {"frozen": False}
 
 
 class RasterDefinition(BaseModel):
@@ -90,21 +100,104 @@ class RasterDefinition(BaseModel):
     row_defs: list[RasterRowDefinition] = Field(..., alias="rowDefs")
     num_cells: int = Field(..., alias="numCells")
     raster_type: str = Field(..., alias="rasterType")
+    model_config = {"frozen": False}
 
 
 class MaxRasterPosition(BaseModel):
-    file: str
-    coords: list[float]
-    index: int
-    omega: float
+    file: Optional[str]
+    coords: list[float | None]
+    index: Optional[int]
+    omega: Optional[float]
+    model_config = {"frozen": False}
 
 
 class RasterRequestDefinition(BaseRequestDefinition):
-    raster_def: RasterDefinition
+    raster_def: RasterDefinition = Field(..., alias="rasterDef")
     max_raster: MaxRasterPosition
+    model_config = {"frozen": False}
 
 
 RequestDefinition = Union[StandardRequestDefinition, RasterRequestDefinition]
+
+
+class StandardResult(BaseModel):
+    time: Optional[float] = None
+    result_time: Optional[datetime] = None
+    diffraction_images: Optional[str] = None
+    fast_dp_row: Optional[List] = None
+    auto_proc_row: Optional[List] = None
+    model_config = {"frozen": False}
+
+    @model_validator(mode="before")
+    def convert_epoch_to_datetime(cls, values: Any) -> Any:
+        # Access other fields using info.data
+        if values.get("result_time") is None and values.get("time") is not None:
+            values["result_time"] = datetime.fromtimestamp(values["time"])
+        return values
+
+
+class RasterCellData(BaseModel):
+    image: Tuple[str, int]
+    spot_count: float
+    spot_count_no_ice: float
+    d_min: float
+    d_min_method_1: Optional[float] = None
+    d_min_method_2: Optional[float] = None
+    total_intensity: float
+    cell_map_key: str = Field(..., alias="cellMapKey")
+    model_config = {"frozen": False}
+
+    @field_validator("image")
+    def validate_and_convert_path(cls, value):
+        # Convert the first element (path string) to a Path object
+        path_str, some_int = value
+        value = (Path(path_str), some_int)
+        return value
+
+
+class RasterCellCollection(BaseModel):
+    type: str
+    cells: List[RasterCellData] = Field(..., alias="resultObj")
+    model_config = {"frozen": False}
+
+
+class RasterResultData(BaseModel):
+    sample_id: UUID
+    parent_request_id: UUID = Field(..., alias="parentReqID")
+    cell_map: Dict[str, Point3D] = Field(..., alias="rasterCellMap")
+    cell_data_collection: RasterCellCollection = Field(..., alias="rasterCellResults")
+    model_config = {"frozen": False}
+
+
+class RasterResult(BaseModel):
+    uid: UUID
+    time: float
+    result_time: Optional[datetime] = None
+    provenance: Dict[str, Any]
+    result_type: str
+    owner: str
+    sample: UUID
+    request: UUID
+    data: RasterResultData = Field(..., alias="result_obj")
+    plot_image: Optional[str] = None
+    jpeg_image: Optional[str] = None
+    top_frames: Optional[str] = None
+    hist_mean_neighbor_dist: Optional[str] = None
+    hist_resolutions: Optional[str] = None
+    hist_spot_count: Optional[str] = None
+
+    model_config = {"frozen": False}
+
+    @model_validator(mode="before")
+    def convert_epoch_to_datetime(cls, values: Any) -> Any:
+        # Access other fields using info.data
+        if values.get("result_time") is None and values.get("time") is not None:
+            values["result_time"] = datetime.fromtimestamp(values["time"])
+        return values
+
+
+Result = Union[StandardResult, RasterResult]
+# Result = RasterResult
 
 
 class Request(BaseModel):
@@ -116,14 +209,43 @@ class Request(BaseModel):
     seq_num: int
     priority: int
     request_type: RequestType
-    request_def: RequestDefinition
+    request_def: RequestDefinition = Field(..., alias="request_obj")
     owner: str
     proposal_id: str = Field(..., alias="proposalID")
+    result: Optional[Result] = None
 
-    @validator("request_time", always=True)
-    def convert_epoch_to_datetime(cls, v, values):
-        epoch_time = values.get("time")
-        if epoch_time is not None:
-            # Convert float epoch time to datetime
-            return datetime.fromtimestamp(epoch_time)
-        return v
+    model_config = {"frozen": False}
+
+    @model_validator(mode="before")
+    def convert_epoch_to_datetime(cls, values: Any) -> Any:
+        # Access other fields using info.data
+        if values.get("request_time") is None and values.get("time") is not None:
+            values["request_time"] = datetime.fromtimestamp(values["time"])
+        return values
+
+
+class Sample(BaseModel):
+    uid: UUID
+    name: str
+    time: float
+    create_time: Optional[datetime] = None
+    container: UUID
+    owner: str
+    kind: str
+    proposal_id: str = Field(..., alias="proposalID")
+    model: str
+    sequence: Optional[str | int] = None
+    request_count: int
+    model_config = {"frozen": False}
+
+
+class SampleData(BaseModel):
+    sample: Sample
+    rasters: Dict[UUID, Dict[UUID, Request]]
+    standard: Dict[UUID, Request]
+    model_config = {"frozen": False}
+
+
+class CollectionData(BaseModel):
+    samples: Dict[str, SampleData]
+    model_config = {"frozen": False}
